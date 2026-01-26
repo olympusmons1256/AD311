75d74e1b12b77b8f13187312d067986112e45c61
olympusmons1256 <142126541+olympusmons1256@users.noreply.github.com>
2026-01-25 23:52:32 -0800

    configuringESLintNodejsProject initial commit

---
+++ configuringESLintNodejsProject/eslint-javascriptTestFile.js
@@ -0,0 +1,9 @@
+const greeting = "Hello world!"
+const name = 'Reid'
+const unused = 42
+
+function sayHello() {
+    console.log(greeting + ' My name is ' + name + '.')
+}
+
+sayHello()
---
+++ configuringESLintNodejsProject/eslint.config.mjs
@@ -0,0 +1,25 @@
+import js from "@eslint/js";
+import globals from "globals";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config"; 
+
+export default defineConfig([
+  {
+    ignores: ["package-lock.json", "eslint.config.mjs"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+    rules: {
+      semi: ["error", "always"], // Enforce semicolons at the end of statements
+      quotes: ["error", "single"], // Enforce single quotes for strings
+    },
+  },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
